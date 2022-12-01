fe3941b7abd0bf3914e1f64e7690b143d2b4fa0a
BekzodGayratov <bekzodflutter@gmail.com>
2022-11-30 23:19:24 -0600

    localizations

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:telelon/core/router/router.dart';
 import 'package:telelon/core/theme/theme.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  runApp(EasyLocalization(
+      saveLocale: true,
+      startLocale: const Locale('uz'),
+      supportedLocales: const [Locale('uz'), Locale('ru')],
+      path: "lib/core/lang",
+      child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -12,6 +20,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
       themeMode: ThemeMode.light,
       theme: TelelonTheme.themeData,
       debugShowCheckedModeBanner: false,
